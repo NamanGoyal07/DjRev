e8f613669797f00c0e79a7613452aae4327b0120
NamanGoyal07 <naman18@iitg.ac.in>
2021-02-21 13:56:06 +0530

    Temp add

--- calc/views.py
+++ calc/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World");
+    return render(request, 'home.html', {'name': 'Naman'})
+
 
 
