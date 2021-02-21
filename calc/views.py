a5490950634e5ac16b6e557dea360a97a3643cdf
NamanGoyal07 <naman18@iitg.ac.in>
2021-02-21 14:31:01 +0530

    Add

--- calc/views.py
+++ calc/views.py
@@ -6,5 +6,11 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html', {'name': 'Naman'})
 
-
+def add(request):
+    val1 = request.GET['num1']
+    val1 = int(val1)
+    val2 = request.GET['num2']
+    val2 = int(val2)
+    res = val1+val2
+    return render(request, 'result.html',{'result':res})
 
